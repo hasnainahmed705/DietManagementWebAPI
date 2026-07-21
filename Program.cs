6bb5ef15fc627527af0bbb93c32d56ec55c7a169
Hasnain Ahmed <hasnainahmed705@gmail.com>
2026-07-21 10:57:12 +0500

    fdsdas

--- Program.cs
+++ Program.cs
@@ -82,15 +82,12 @@ var app = builder.Build();
 
 // ---- Middleware Pipeline ----
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Diet Management API v1");
-        c.RoutePrefix = "swagger";
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Diet Management API v1");
+    c.RoutePrefix = "swagger";
+});
 
 // app.UseHttpsRedirection();   // Disable for Render / reverse proxy
 
