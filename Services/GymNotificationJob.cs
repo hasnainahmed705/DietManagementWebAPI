674714c1877bbc2d4f8be6d39423f6d41489beea
Hasnain Ahmed <hasnainahmed705@gmail.com>
2026-08-17 15:52:00 +0500

    dssadasdasd

--- Services/GymNotificationJob.cs
+++ Services/GymNotificationJob.cs
@@ -142,7 +142,7 @@ namespace DietManagementWebAPI.Services
                         "10:00 AM",
                         "12:00 PM",
                         "02:00 PM",
-                        "04:00 PM",
+                        "03:54 PM",
                         "06:00 PM",
                         "08:00 PM",
                         "10:00 PM"
